0891f46dbfc201bf284a6a282e56cdd3bb716021
Israel <tucorreo@gmail.com>
2026-04-29 17:55:30 -0600

    fix port

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ app.post('/webhook', (req, res) => {
     `);
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Servidor corriendo en puerto 3000');
 });
