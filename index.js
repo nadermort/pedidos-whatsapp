475969b12c61c018abff4c3e85cbd3df4150cab9
Israel <tucorreo@gmail.com>
2026-04-29 17:31:19 -0600

    primer commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.post('/webhook', (req, res) => {
+    const mensaje = req.body.Body;
+    const numero = req.body.From;
+
+    console.log('Nuevo pedido recibido');
+    console.log('De:', numero);
+    console.log('Mensaje:', mensaje);
+
+    res.set('Content-Type', 'text/xml');
+    res.send(`
+        <Response>
+            <Message>Pedido recibido. En breve lo procesamos.</Message>
+        </Response>
+    `);
+});
+
+app.listen(3000, () => {
+    console.log('Servidor corriendo en puerto 3000');
+});
