e15491e2793c9b7eee268bc79eca2c46ae061120
Israel <tucorreo@gmail.com>
2026-04-29 18:11:38 -0600

    fix host binding

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.post('/webhook', (req, res) => {
     `);
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Servidor corriendo en puerto 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
 });
